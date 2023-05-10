ccde14a871436de1c9fe1265566e4e233d04d9c8
wendyhamel <whamel@it4web.nl>
2023-05-10 16:59:14 +0200

    Styling added.

--- src/results.js
+++ src/results.js
@@ -4,22 +4,30 @@ window.data = function () {
 			{
 				"category": "Reaction",
 				"score": 80,
-				"icon": "./assets/images/icon-reaction.svg"
+				"icon": "./assets/images/icon-reaction.svg",
+				"textColor": "text-primary-red",
+				"bgColor": "bg-primary-red/5"
 			},
 			{
 				"category": "Memory",
 				"score": 92,
-				"icon": "./assets/images/icon-memory.svg"
+				"icon": "./assets/images/icon-memory.svg",
+				"textColor": "text-primary-orange",
+				"bgColor": "bg-primary-orange/5"
 			},
 			{
 				"category": "Verbal",
 				"score": 61,
-				"icon": "./assets/images/icon-verbal.svg"
+				"icon": "./assets/images/icon-verbal.svg",
+				"textColor": "text-primary-teal",
+				"bgColor": "bg-primary-teal/5"
 			},
 			{
 				"category": "Visual",
 				"score": 72,
-				"icon": "./assets/images/icon-visual.svg"
+				"icon": "./assets/images/icon-visual.svg",
+				"textColor": "text-primary-blue",
+				"bgColor": "bg-primary-blue/5"
 			}
 		]
 	}
--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -5,8 +5,23 @@ tailwind.config = {
 		},
 		extend: {
 			colors: {
-				'color': {
-					'tint': 'hsl(0, 0%, 0%)'
+				'primary': {
+					'red': 'hsl(0, 100%, 67%)',
+					'orange': 'hsl(39, 100%, 56%)',
+					'teal': 'hsl(166, 100%, 37%)',
+					'blue': 'hsl(234, 85%, 45%)',
+				},
+				'gradient': {
+					'slate-blue': 'hsl(252, 100%, 67%)', //background
+					'royal-blue': 'hsl(241, 81%, 54%)', //background
+					'violet-blue': 'hsla(256, 72%, 46%, 1)', //circle
+					'persian-blue': 'hsla(241, 72%, 46%, 0)', //circle
+				},
+				'neutral': {
+					'white': 'hsl(0, 0%, 100%)',
+					'pale-blue': 'hsl(221, 100%, 96%)',
+					'pale-lavender': 'hsl(241, 100%, 89%)',
+					'gray-blue': 'hsl(224, 30%, 27%)',
 				}
 			}
 		}
