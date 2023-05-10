5d72f6ae19b5c70d8ddbc7620eaa314ea13a4c8b
wendyhamel <whamel@it4web.nl>
2023-05-10 15:04:05 +0200

    Setup tailwind and alpine. Added html structure and data via js.

---
+++ src/tailwind.config.js
@@ -0,0 +1,14 @@
+tailwind.config = {
+	theme: {
+		fontFamily: {
+			'sans': ['Hanken Grotesk', 'sans-serif']
+		},
+		extend: {
+			colors: {
+				'color': {
+					'tint': 'hsl(0, 0%, 0%)'
+				}
+			}
+		}
+	}
+}
